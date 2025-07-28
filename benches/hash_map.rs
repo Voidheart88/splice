1a7c35f64b4363bcc435d109d7d1df7c4ea1d570
Dominik Stolte <dominikstolte@aol.com>
2025-07-28 20:18:09 +0200

    added nohash hasher

--- benches/hash_map.rs
+++ benches/hash_map.rs
@@ -3,6 +3,9 @@ use rand::seq::SliceRandom;
 use rand::Rng;
 use std::collections::HashMap;
 
+use nohash_hasher::BuildNoHashHasher;
+use nohash_hasher::IsEnabled;
+
 const MAP_SIZE: usize = 10_000;
 const LARGE_MAP_SIZE: usize = 1_000_000;
 
@@ -102,18 +105,25 @@ pub fn bench_hashmap_get_mut(c: &mut Criterion) {
     group.finish();
 }
 
+// Kombiniert zwei u32 zu einem u64
+fn make_key(row: u32, col: u32) -> u64 {
+    ((row as u64) << 32) | (col as u64)
+}
+
 pub fn bench_nohash_hashmap(c: &mut Criterion) {
     let mut group = c.benchmark_group("No_hash_HashMap");
 
     group.bench_function(format!("insert_{}", MAP_SIZE), |b| {
-        let mut map = HashMap::with_capacity(MAP_SIZE);
-        let mut keys_to_get: Vec<(u64, u64)> = Vec::with_capacity(MAP_SIZE);
+        let mut map: HashMap<u64, u64, BuildNoHashHasher<u64>> = 
+            HashMap::with_hasher(BuildNoHashHasher::default());
 
-        for _ in 0..MAP_SIZE {
-            let key_row = rand::rng().random();
-            let key_col = rand::rng().random();
-            map.insert((key_row, key_col), key_row);
-            keys_to_get.push((key_row, key_col));
+        let mut keys_to_get: Vec<u64> = Vec::with_capacity(MAP_SIZE);
+
+        for idx in 0..MAP_SIZE {
+            let key_row =  rand::rng().random();
+            let key_col =  rand::rng().random();
+            map.insert(make_key(key_row,key_col),idx as u64);
+            keys_to_get.push(make_key(key_row,key_col) as u64);
         }
 
         keys_to_get.shuffle(&mut rand::rng());
@@ -126,15 +136,17 @@ pub fn bench_nohash_hashmap(c: &mut Criterion) {
         });
     });
 
-    group.bench_function(format!("get_mut_{}", LARGE_MAP_SIZE), |b| {
-        let mut map = HashMap::with_capacity(MAP_SIZE);
-        let mut keys_to_get: Vec<(u64, u64)> = Vec::with_capacity(MAP_SIZE);
+    group.bench_function(format!("insert_{}", LARGE_MAP_SIZE), |b| {
+        let mut map: HashMap<u64, u64, BuildNoHashHasher<u64>> = 
+            HashMap::with_hasher(BuildNoHashHasher::default());
 
-        for _ in 0..MAP_SIZE {
-            let key_row = rand::rng().random();
-            let key_col = rand::rng().random();
-            map.insert((key_row, key_col), key_row);
-            keys_to_get.push((key_row, key_col));
+        let mut keys_to_get: Vec<u64> = Vec::with_capacity(LARGE_MAP_SIZE);
+
+        for idx in 0..LARGE_MAP_SIZE {
+            let key_row =  rand::rng().random();
+            let key_col =  rand::rng().random();
+            map.insert(make_key(key_row,key_col),idx as u64);
+            keys_to_get.push(make_key(key_row,key_col) as u64);
         }
 
         keys_to_get.shuffle(&mut rand::rng());
--- benches/performance.rs
+++ benches/performance.rs
@@ -2,8 +2,8 @@ mod diode;
 mod faer;
 mod hash_map;
 mod resistor;
-mod rsparse;
-mod nalgebra;
+//mod rsparse;
+//mod nalgebra;
 
 use criterion::{criterion_group, criterion_main};
 
@@ -11,15 +11,22 @@ use diode::diode_pairs_benchmark;
 use diode::diode_triples_benchmark;
 use faer::{faer_insert_a_1000_benchmark, faer_insert_a_benchmark, faer_insert_b_benchmark};
 use hash_map::{bench_hashmap_get_mut, bench_hashmap_insert};
-use nalgebra::{
-    nalgebra_insert_a_1000_benchmark, nalgebra_insert_a_benchmark, nalgebra_insert_b_benchmark,
-};
+//use nalgebra::{
+//    nalgebra_insert_a_1000_benchmark, nalgebra_insert_a_benchmark, nalgebra_insert_b_benchmark,
+//};
 use resistor::resistor_triples_benchmark;
-use rsparse::{
-    rsparse_insert_a_1000_benchmark, rsparse_insert_a_benchmark, rsparse_insert_b_benchmark,
-};
 
-criterion_group!(hashmap_benches, bench_hashmap_insert, bench_hashmap_get_mut,);
+use crate::hash_map::bench_nohash_hashmap;
+//use rsparse::{
+//    rsparse_insert_a_1000_benchmark, rsparse_insert_a_benchmark, rsparse_insert_b_benchmark,
+//};
+
+criterion_group!(
+    hashmap_benches, 
+    bench_hashmap_insert, 
+    bench_hashmap_get_mut,
+    bench_nohash_hashmap,
+);
 
 criterion_group!(
     model_benches,
@@ -32,15 +39,15 @@ criterion_group!(
     backend_benches,
 
     faer_insert_a_benchmark,
-    rsparse_insert_a_benchmark,
-    nalgebra_insert_a_benchmark,
+    //rsparse_insert_a_benchmark,
+    //nalgebra_insert_a_benchmark,
     
     faer_insert_b_benchmark,
-    rsparse_insert_b_benchmark,
-    nalgebra_insert_b_benchmark,
+    //rsparse_insert_b_benchmark,
+    //nalgebra_insert_b_benchmark,
     
     faer_insert_a_1000_benchmark,
-    rsparse_insert_a_1000_benchmark,
-    nalgebra_insert_a_1000_benchmark,
+    //rsparse_insert_a_1000_benchmark,
+    //nalgebra_insert_a_1000_benchmark,
 );
-criterion_main!(backend_benches);
+criterion_main!(hashmap_benches);
